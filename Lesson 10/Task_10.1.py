ec993821e9e3865ed56340c3a0dce674afb8f616
Pitonesku <maksymtishayev@gmail.com>
2026-04-02 20:32:15 +0200

    Lesson 10 in progress. Task 1 ready

---
+++ Lesson 10/Task_10.1.py
@@ -0,0 +1,36 @@
+# ДЗ 10.1. Генераторна функція
+# Реалізуйте генераторну функцію (з використанням оператора yield), яка повертатиме по одному члену числової послідовності, 
+# закон якої задається за допомогою функції користувача. 
+# Крім цього параметром генераторної функції повинні бути значення першого члена прогресії та кількість членів, що видаються послідовності (n). 
+# Генератор повинен зупинити свою роботу з досягнення n-го члена.
+# Підказка: це завдання дуже схоже на нескінченний лічильник з матеріалів лекції! Потрібно лише обмежити кількість видаваних генератором значень!
+
+from inspect import isgenerator
+
+def pow(x):
+    return x**2
+
+def my_generator(start: int, qty: int, func):
+    
+    """
+    Generate list of numbers based on desired function
+    """
+    i = 0 
+    while i < qty:
+        result = start
+        start = func(start)
+        i+=1
+        yield result
+
+my_gen = my_generator(2,5,pow)
+
+for i in my_gen:
+    print(i)
+
+
+gen = my_generator(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
+
+
---
+++ Lesson 10/Task_10.2.py
@@ -0,0 +1,35 @@
+# ДЗ 10.2. Знайти перше слово
+# Напишіть функцію first_word, яка у переданому рядку знайде її перше слово.
+# При розв'язанні задачі зверніть увагу на наступні моменти:
+#     У рядку можуть зустрічаються крапки та/або коми
+#     Рядок може починатися з літери або, наприклад, з пробілу або точки
+#     У слові може бути апостроф і він є частиною слова
+#     Весь текст може бути представлений лише одним словом та все
+
+# Вхідні параметри: Рядок.
+# Вихідні параметри: Рядок.
+
+import string
+
+def first_word(text: str) -> str:
+    """
+    Find first word in the text
+    """
+    text = (text.strip().split((" ")))
+    print(text)
+
+    for word in text:
+        if any(char.isalpha() for char in word):
+            first_word = word
+            break
+    first_word = "".join(filter(lambda char: char.isalpha() or char == "'" or char not in string.punctuation, first_word))
+    return first_word
+
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
